ebbb170db17fed528fbaf524a874e3c3e21e8eba
Dai Jian <daijian@stumail.ysu.edu.cn>
2024-08-12 13:52:51 +0800

    remove debug log.

--- jbag/transforms/transforms.py
+++ jbag/transforms/transforms.py
@@ -19,23 +19,12 @@ class Transform(ABC):
 
 
 class RandomTransform(Transform, ABC):
-    def __init__(self, keys, apply_probability, log, log_name):
+    def __init__(self, keys, apply_probability):
         super().__init__(keys)
         self.apply_probability = apply_probability
-        self.log = log
-        self.log_name = log_name
-        self.total_count = 0
-        self.changed_count = 0
 
     def __call__(self, data):
-        self.total_count += 1
         if np.random.random_sample() < self.apply_probability:
-            # return self._call_fun(data)
-            self.changed_count += 1
-            data = self._call_fun(data)
-            for key in self.keys:
-                changed_data = data[key]
-                self.log.add_image(f'log_name/{key}/{self.changed_count}', changed_data.squeeze(0))
-            return data
+            return self._call_fun(data)
         else:
             return data
