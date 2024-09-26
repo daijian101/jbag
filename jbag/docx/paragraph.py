b9481a0ead595b596217ef07ed8147014a79fa37
Dai Jian <daijian@stumail.ysu.edu.cn>
2024-09-26 10:42:57 +0800

    Refactor 'add_paragraph'.

--- jbag/docx/paragraph.py
+++ jbag/docx/paragraph.py
@@ -4,9 +4,13 @@ from docx import Document
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 
 
-def add_paragraph(doc: Type[Document], text=None, style=None,
-                  alignment: Type[WD_PARAGRAPH_ALIGNMENT] = WD_PARAGRAPH_ALIGNMENT.JUSTIFY):
+def add_paragraph(doc: Type[Document],
+                  text=None,
+                  alignment: Type[WD_PARAGRAPH_ALIGNMENT] = WD_PARAGRAPH_ALIGNMENT.JUSTIFY,
+                  style=None
+                  ):
     if text is None:
         text = ''
-    p = doc.add_paragraph(text=text, style=style)
-    p.alignment = alignment
+    p = doc.add_paragraph(text='', style=style)
+    if alignment is not None:
+        p.alignment = alignment
