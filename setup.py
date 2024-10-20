43b8ab37f678f0a2ccc12096e37c36359c42d273
Dai Jian <jianacad@outlook.com>
2024-10-20 19:35:45 +0800

    Update version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='jbag',
-    version='4.0.2',
+    version='4.0.3',
     author='Dai Jian',
     author_email='daijian@stumail.ysu.edu.cn',
     description='Tools for medical image processing and deap learning.',
